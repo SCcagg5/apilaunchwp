d52f3870a5709b511b1c7c7eb052d196a4d0be4f
sccagg5 <sccagg5@gmail.com>
2019-08-14 13:54:59 +0200

    src

--- api/Controller/routesfunc.py
+++ api/Controller/routesfunc.py
@@ -2,7 +2,7 @@ from Model.basic import check
 from Object.evts import evts
 
 def deploy(cn, nextc):
-    err = check.contain(cn.pr, ["number", "login")
+    err = check.contain(cn.pr, ["number", "login"])
     if not err[0]:
         return cn.toret.add_error(err[1], err[2])
     err = evts.run(cn.pr["number"], cn.pr["login"])
