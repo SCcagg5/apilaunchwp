8fb4b086590e053e52a50359b56806789c179290
SCcagg5 <sccagg5@gmail.com>
2019-09-02 20:55:10 +0200

    Update evts.py

--- api/Object/evts.py
+++ api/Object/evts.py
@@ -8,9 +8,15 @@ import time
 
 class evts:
     def run(x, login):
+        name = str(login) + "-" +str(x)
         commands = [
-        "docker run -e MYSQL_ROOT_PASSWORD=password -e MYSQL_DATABASE=wordpress --name "+login+"-"+str(x)+"_db -v /srv/isginnovation/"+login+"/"+str(x)+"/database:/var/lib/mysql --restart always -d mariadb:latest",
-        "docker run -e WORDPRESS_DB_PASSWORD=password -e VIRTUAL_HOST="+login+"-"+str(x)+".isginnovation.fr -e LETSENCRYPT_HOST="+login+"-"+str(x)+".isginnovation.fr --restart always --name "+login+"-"+str(x)+"_wp --link "+login+"-"+str(x)+"_db:mysql -v /srv/isginnovation/"+login+"/"+str(x)+"/wordpress:/var/www/html -d wordpress"]
+        "docker run -e MYSQL_ROOT_PASSWORD=password -e MYSQL_DATABASE=wordpress --name "+name+"_db -v /srv/isginnovation/"+login+"/"+str(x)+"/database:/var/lib/mysql --restart always -d mariadb:latest",
+        "docker run -e WORDPRESS_DB_PASSWORD=password -e VIRTUAL_HOST="+name+".isginnovation.fr -e LETSENCRYPT_HOST="+name+".isginnovation.fr --restart always --name "+name+"_wp --link "+login+"-"+str(x)+"_db:mysql -v /srv/isginnovation/"+login+"/"+str(x)+"/wordpress:/var/www/html -d wordpress",
+        "sleep 10",
+        "docker exec "+name+"_wp curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar",
+        "docker exec "+name+"_wp chmod +x wp-cli.phar",
+        "docker exec "+name+"_wp mv wp-cli.phar /usr/local/bin/wp-cli"
+        "docker exec "+name+"_wp wp-cli --allow-root core install --url="+name+".isginnovation.fr --title='"+name+"' --admin_user="+login+" --admin_password=1q2W3e --admin_email=eliot.courtel@gmail.com"]
         globalcom = ""
         for i in commands:
             globalcom += str(i) + " ; "
